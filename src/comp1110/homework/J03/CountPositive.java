38c7e16fa5ecf59a8301c6bd2410dc176356b961
Harold Gao <harold.gao205@gmail.com>
2024-07-28 13:18:47 +1000

    Implemented CountPositive class

---
+++ src/comp1110/homework/J03/CountPositive.java
@@ -0,0 +1,13 @@
+package comp1110.homework.J03;
+
+public class CountPositive {
+
+    static int countPositive(int[] numbers){
+        int positives = 0;
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] > 0) positives++;
+        }
+        return positives;
+    }
+
+}
